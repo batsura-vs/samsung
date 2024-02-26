867f624c266322518bffaea86c5a2f219a271407
voven4ek <batsura.vs@gmail.com>
2024-02-26 21:35:01 +0300

    update

--- android_apps/homeworks/GeoAlarm/app/src/main/java/com/voven4ek/geoalarm/ui/components/pages/Home.kt
+++ android_apps/homeworks/GeoAlarm/app/src/main/java/com/voven4ek/geoalarm/ui/components/pages/Home.kt
@@ -2,14 +2,25 @@ package com.voven4ek.geoalarm.ui.components.pages
 
 import androidx.compose.animation.core.animateFloatAsState
 import androidx.compose.foundation.background
-import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Menu
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Card
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.ListItem
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -18,9 +29,8 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.alpha
-import androidx.compose.ui.draw.blur
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
@@ -47,10 +57,8 @@ fun Home(
 ) {
     var helperActive by remember { mutableStateOf(false) }
     val helperSize by animateFloatAsState(
-        targetValue = if (helperActive) 0.4f else 2f, label = "helper size"
+        targetValue = if (helperActive) 2f else 0.4f, label = "helper size"
     )
-
-
     MapComponent(
         modifier = Modifier.fillMaxSize(), model = model, isPreview = isPreview
     )
@@ -58,18 +66,60 @@ fun Home(
         Spacer(modifier = Modifier.weight(1f))
         Box(
             modifier = Modifier
+                .padding(4.dp)
+                .clip(RoundedCornerShape(16.dp))
+                .background(MaterialTheme.colorScheme.background.copy(0.95f))
                 .fillMaxWidth()
-                .clip(RoundedCornerShape(topEnd = 16.dp, topStart = 16.dp))
-                .alpha(0.5f)
-                .background(Color.Black)
-                .clickable {
-                    helperActive = !helperActive
-                },
+                .weight(helperSize),
         ) {
-            Column (
+            Column(
                 modifier = Modifier.padding(16.dp)
             ) {
-                Text(text = "Example")
+                Row(
+                    modifier = Modifier
+                        .fillMaxWidth(),
+                ) {
+                    Text(
+                        text = "Settings",
+                        fontSize = MaterialTheme.typography.titleLarge.fontSize,
+                        modifier = Modifier.align(Alignment.CenterVertically),
+                    )
+                    Spacer(modifier = Modifier.weight(1f))
+                    IconButton(
+                        onClick = {
+                            helperActive = !helperActive
+                        },
+                        modifier = Modifier.align(Alignment.CenterVertically),
+                    ) {
+                        Icon(Icons.Filled.Menu, contentDescription = "Settings")
+                    }
+                }
+                LazyColumn(
+                    modifier = Modifier
+                        .weight(1f),
+                    content = {
+                        items(listOf("1", "2", "3")) {
+                            Card(
+                                modifier = Modifier.padding(4.dp)
+                            ) {
+                                ListItem(
+                                    headlineContent = {
+                                        Text(text = "Item $it")
+                                    },
+                                )
+                            }
+                        }
+                    },
+                )
+                Row {
+                    Spacer(modifier = Modifier.weight(1f))
+                    Button(
+                        onClick = { /*TODO*/ },
+                        colors = ButtonDefaults.elevatedButtonColors(),
+                    ) {
+                        Text(text = "Save")
+                    }
+                }
             }
         }
     }
@@ -121,7 +171,7 @@ fun MapComponent(
                         return true
                     }
                 })
-
+                setBuiltInZoomControls(false)
                 overlays.add(events)
                 overlays.add(marker)
                 Configuration.getInstance().userAgentValue = "GeoAlarm"
